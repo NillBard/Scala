e74326abe70088bf5d4a850fc38d144df3041897
Daniil <dan.bardyuk@yandex.ru>
2021-11-12 12:20:31 +0300

    add lab2

---
+++ lab3/src/main/scala/main.scala
@@ -0,0 +1,20 @@
+object main {
+  def main(args: Array[String]):Unit = {
+      println("------------------Compositions-------------------------")
+      println("testCompose", Compositions.testCompose((a:Int)=> a*3)((b:Int)=>b*2)((c:Int)=>Math.pow(c,2)/100)(10))
+      println("testMapFlatMap", Compositions.testMapFlatMap((a: Int) =>
+        Some(a.toString))((b: String) => Some(b.isBlank))((c: Boolean) => c.toString)(Some(0)))
+      println("testForComprehension", Compositions.testForComprehension((a: Int) =>
+        Some(a.toString))((b: String) => Some(!b.isBlank))((c: Boolean) => c.toString)(Some(5)))
+      println("------------------Recursive-Functions-------------------------")
+      println ("testReverse",
+        RecursiveFunctions.testReverse(Cons(1,Cons(2,Cons(3, Nil()))))
+      )
+      println("testMap", RecursiveFunctions.testMap(Cons(1,Cons(2,Cons(3, Nil()))),(a:Int)=>a*2))
+      println("testFlatMap", RecursiveFunctions.testFlatMap(Cons(1,Cons(2,Cons(3, Nil()))),(a:Int)=>Cons(a*2,Nil())))
+      println("------------------Recursive-Functions-------------------------")
+
+      println("testListIntEmpty", RecursiveData.testListIntEmpty(Cons(1,Nil())))
+      println("testListIntHead", RecursiveData.testListIntHead(Cons(1,Nil())))
+  }
+}
